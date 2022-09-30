Save raw corrected: force_update_info when adding ET channels. grand average implemented. Save of Epoched and Evoked implemented. Removed new item_pos

preproc_functions.py
@@ -84,8 +84,7 @@ def blinks_to_nan(meg_pupils_data_raw, meg_gazex_data_scaled, meg_gazey_data_sca
     if missing[-1] == 1:
         missing_start = missing_start[:-1]
 
-    # Remove blinks
-    # First, fill missing values (and suroundings) with nan
+    # Remove blinks by filling missing values (and suroundings) with nan
     for i in range(len(missing_start)):
         blink_interval = np.arange(missing_start[i] - start_interval_samples, missing_end[i] + end_interval_samples)
         meg_gazex_data_clean[blink_interval] = float('nan')
@@ -610,7 +609,7 @@ def target_vs_distractor(fixations, bh_data, subject, distance_threshold=100, sc
     print('Identifying fixated items')
 
     # Load items data
-    items_pos_path = subject.item_pos_path
+    items_pos_path = paths().item_pos_path()
     items_pos = pd.read_csv(items_pos_path)
 
     # Rescale images from original resolution to screen resolution to match fixations scale

preprocessing.py
@@ -98,15 +98,15 @@ def preprocess(subject, plot=False):
         raw_et.rename_channels({ch_name: new_name})
 
     # Pick data from MEG channels and other channels of interest
-    channel_indices = mne.pick_types(raw.info, meg=True)
-    channel_indices = np.append(channel_indices, mne.pick_channels(raw.info['ch_names'], ['UPPT001']))
-    raw.pick(channel_indices)
+    channel_idx = mne.pick_types(raw.info, meg=True)
+    channel_idx = np.append(channel_idx, mne.pick_channels(raw.info['ch_names'], ['UPPT001', 'UADC001-4123', 'UADC002-4123', 'UADC013-4123']))
+    raw.pick(channel_idx)
 
     # save to original raw structure (requires to load data)
     print('Loading MEG data')
     raw.load_data()
     print('Adding new ET channels')
-    raw.add_channels([raw_et])
+    raw.add_channels([raw_et], force_update_info=True)
     del (raw_et)
 
     #---------------- Save preprocesed data ----------------#