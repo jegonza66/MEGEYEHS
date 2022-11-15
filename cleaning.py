05379f4da4d9499223679c64dcecdbe2874aac48
Joac <joaquin.gonzalez6693@gmail.com>
2022-11-15 15:19:31 -0300

    continued cleaning module to annotate cfg events

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import numpy as np
 from mne.preprocessing import annotate_muscle_zscore
 import mne
@@ -18,35 +17,40 @@ for subject_code in exp_info.subjects_ids:
     subject = load.preproc_subject(exp_info=exp_info, subject_code=subject_code)
     meg_data = subject.load_preproc_meg()
 
-    eog_events = mne.preprocessing.find_eog_events(meg_data, ch_name='EOG001')
+    # Visual annotation
+    # fig = meg_data.plot(start=0, duration=6)
+    # fig.fake_keypress('a')
 
-    n_blinks = len(eog_events)
-    onset = eog_events[:, 0] / meg_data.info['sfreq'] - 0.25
-    duration = np.repeat(0.5, n_blinks)
-    description = ['blink'] * n_blinks
+    # Annotate cardiac artifacts
+    ecg_events = mne.preprocessing.find_ecg_events(meg_data)[0]
+
+    n_beats = len(ecg_events)
+    onset = ecg_events[:, 0] / meg_data.info['sfreq'] - 0.25
+    duration = np.repeat(0.5, n_beats)
+    description = ['beats'] * n_beats
     orig_time = meg_data.info['meas_date']
-    annotations_blink = mne.Annotations(onset, duration, description, orig_time)
+    annotations_heart = mne.Annotations(onset, duration, description, orig_time)
+
+    # Make epochs
+    ecg_epochs = mne.preprocessing.create_ecg_epochs(meg_data)
+    ecg_epochs.plot_image(combine='mean')
+
+    # Average evoked
+    ecg_evoked = ecg_epochs.average()
+    ecg_evoked.apply_baseline((None, -0.2))
+    ecg_evoked.plot_joint()
 
 
-
-    # Muscle artifacts
+    #--------- Muscle artifacts ---------#
     threshold_muscle = 10
     annotations_muscle, scores_muscle = annotate_muscle_zscore(meg_data, ch_type="mag", threshold=threshold_muscle,
                                                                min_length_good=0.2, filter_freq=[110, 140])
 
     # Include annotations in data
-    meg_data.set_annotations(annotations_blink + annotations_muscle)
+    meg_data.set_annotations(annotations_heart + annotations_muscle)
 
-    meg_data.plot(start=50);
-
-    # Set the channel type as 'eog'
-    meg_data.set_channel_types({'EOG001': 'eog'})
-    meg_data.set_channel_types({'EOG002': 'eog'})
-
-    eog_picks = mne.pick_types(meg_data.info, meg=False, eog=True)
-
-    scl = dict(eog=500e-6)
-    meg_data.plot(order=eog_picks, scalings=scl, start=50)
+    # Plot
+    meg_data.plot(start=50)
 
     path_file_results = os.path.join(paths().save_path(), 'training_rawann-1.fif')
     path_file_results
