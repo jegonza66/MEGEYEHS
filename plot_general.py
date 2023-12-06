Implemented default baseline as (None, 0) in epoch_evoked_ga Corrected default amplitude rejection in epoching in functions analysis. Implemented n_jobs in time frequency computation in time_frequency and funcionts_analysis. Implemented option for source estimate from timefreq, defining if power output is epoched or averaged, complex or not, ande defined paths accordingly. Separated ITC computation and plotting in TF_diff and time_frequency. Removed spureous plots from time_frequency and TF. Implemented min colorbar values in plotjoint_picks in plot_general

plot_general.py
@@ -552,12 +552,14 @@ def tfr_plotjoint_picks(tfr, plot_baseline=None, bline_mode=None, plot_xlim=(Non
     # Get min and max from all topoplots
     if vmin == None and vmax == None:
         maxs = []
+        mins = []
         for timefreq in timefreqs:
             tfr_crop = tfr_topo.copy().crop(tmin=timefreq[0], tmax=timefreq[0], fmin=timefreq[1], fmax=timefreq[1])
             data = tfr_crop.data.ravel()
             maxs.append(data.max())
+            mins.append(data.min())
         vmax = np.max(maxs)
-        vmin = -vmax
+        vmin = np.min(mins)
 
     # Get topo axes and overwrite
     topo_axes = fig.axes[1:-1]