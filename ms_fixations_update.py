ace5bc44ac499eced576d6dc737c53bf4f30a976
Joac <joaquin.gonzalez6693@gmail.com>
2024-04-16 09:48:35 -0300

    Included trials MS screen scanpaths plots

--- ms_fixations_update.py
+++ ms_fixations_update.py
@@ -8,15 +8,14 @@ from paths import paths
 import save
 import mne
 import pandas as pd
+import plot_preproc
 
 # Load experiment info
 exp_info = setup.exp_info()
 config = setup.config()
-# Run plots
-plot = True
 
 # Run
-for subject_code in exp_info.subjects_ids:
+for subject_code in exp_info.subjects_ids[1:]:
 
     # ---------------- Load data ----------------#
     # Define subject
@@ -24,14 +23,23 @@ for subject_code in exp_info.subjects_ids:
     subject_preproc = load.preproc_subject(exp_info=exp_info, subject_code=subject_code)
 
     # Load MEG data
-    ica_data = load.ica_data(subject=subject_ica)
-    preproc_data = subject_preproc.load_preproc_meg_data()
+    ica_data = load.ica_data(subject=subject_ica, preload=True)
+    preproc_data = subject_preproc.load_preproc_meg_data(preload=True)
 
     # Get fixations from subject object
     fixations = subject_ica.fixations
     # Detect fixations to MS items
-    raw2, subject, ms_items_pos = functions_preproc.ms_items_fixations(fixations=copy.copy(subject_ica.fixations), subject=copy.copy(subject_ica),
+    raw, subject, ms_items_pos = functions_preproc.ms_items_fixations(fixations=copy.copy(subject_ica.fixations), subject=copy.copy(subject_ica),
                                                                       raw=ica_data.copy(), distance_threshold=70)
+    print('Getting et data from meg...')
+    et_channels_meg = raw.get_data(picks=['ET_gaze_x', 'ET_gaze_y', 'ET_pupils'])
+
+    print('Plotting scanpaths and trials gaze screens')
+    for trial_idx in range(len(subject.bh_data)):
+        print(f'\rTrial {trial_idx + 1}', end='')
+
+        plot_preproc.ms_scanpath(raw=raw, subject=subject, ms_items_pos=ms_items_pos, et_channels_meg=et_channels_meg, trial_idx=trial_idx)
+    print('\n')
 
     # Overwrite subjects fixations
     subject_ica.fixations = subject.fixations
@@ -63,10 +71,11 @@ for subject_code in exp_info.subjects_ids:
     # Save MEG
     preproc_meg_data_fname = f'Subject_{subject.subject_id}_meg.fif'
     preproc_data.save(preproc_save_path + preproc_meg_data_fname, overwrite=True)
+
     path_file = os.path.join(save_path_ica, f'Subject_{subject.subject_id}_ICA.fif')
     ica_data.save(path_file, overwrite=True)
 
-    # Save fixations ans saccades
+    # Save fixations and saccades
     fixations_new.to_csv(preproc_save_path + 'fixations.csv')
 
     # Save events
