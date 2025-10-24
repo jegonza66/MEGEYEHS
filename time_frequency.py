8cf19445e82f0ba5712b323fef68e2638a70455f
Joac <joaquin.gonzalez6693@gmail.com>
2025-10-24 16:08:43 -0300

    Renamed permutations test on tf data function name

--- time_frequency.py
+++ time_frequency.py
@@ -33,13 +33,13 @@ else:
 use_ica_data = True
 
 # Trial selection and filters parameters. A field with 2 values will compute the difference between the conditions specified
-trial_params = {'epoch_id': 'tgt_fix_ms',  # 'it_sac_ms+tgt_sac_ms', 'it_sac_vs+tgt_sac_vs', 'it_sac_vs', 'tgt_fix_vs', 'sac_emap', 'hl_start'
+trial_params = {'epoch_id': 'it_fix_ms+tgt_fix_ms',  # 'it_sac_ms+tgt_sac_ms', 'it_sac_vs+tgt_sac_vs', 'it_sac_vs', 'tgt_fix_vs', 'sac_emap', 'hl_start'
                 'corrans': [True, False],
                 'tgtpres': None,
                 'mss': None,
                 'reject': None,
                 'evtdur': None,
-                'rel_sac': None
+                'rel_sac': 'prev'
                 }
 run_comparison = True
 
@@ -421,7 +421,7 @@ for param in param_values.keys():
                     min_sig_chs = len(picks) * significant_channels
 
                     # Run clusters permutations test
-                    clusters_mask, clusters_mask_plot, significant_pvalues = functions_analysis.run_time_frequency_test(data=permutations_test_data_array, pval_threshold=pval_threshold,
+                    clusters_mask, clusters_mask_plot, significant_pvalues = functions_analysis.run_permutations_test_tf(data=permutations_test_data_array, pval_threshold=pval_threshold,
                                                                                                    t_thresh=pval_threshold, n_permutations=n_permutations, min_sig_chs=min_sig_chs)
 
                     # Define image args to plot mask
